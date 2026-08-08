6ec8b2db6553569fbce3ee5e6e195f1dd9422501
PhoenixMiele <danielmiele89@gmail.com>
2026-08-08 15:52:59 +0100

    #9 Implement dog movement

--- Game/scripts/dog/dog.gd
+++ Game/scripts/dog/dog.gd
@@ -1,25 +1,19 @@
 extends CharacterBody2D
 
+@export var speed: float = 200.0
+@export var run_speed: float = 360.0
+@export var deceleration: float = 10.0
+@export var speed_change: float = 8.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+func _physics_process(_delta: float) -> void:
+	# Horizontal movement
+	var direction := Input.get_axis("move_left", "move_right")
+	var current_speed := speed
+	if Input.is_action_pressed("run"):
+		current_speed = run_speed
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * current_speed, speed_change)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, deceleration)
 
 	move_and_slide()
