0b65f94ac46f38a6e18b26b9ab0c0c563aaa83d4
PhoenixMiele <danielmiele89@gmail.com>
2026-08-12 15:41:19 +0100

    #14 Allow Multiple Investigation Points in Scene

--- Game/scripts/creek/creek.gd
+++ Game/scripts/creek/creek.gd
@@ -1,25 +1,19 @@
 extends Node2D
+signal investigations_completed
 var completed_investigations: Array[String] = []
+var investigations_are_complete: bool = false
 
 func _on_investigation_completed(investigation_id: String) -> void:
 	if investigation_id not in completed_investigations:
 		completed_investigations.append(investigation_id)
 		
-	print("Investigation completed: ", investigation_id)
-	if _all_creek_investigations_complete():
-		print("All creek investigations complete.")
+	if _all_creek_investigations_complete() and not investigations_are_complete:
+		investigations_are_complete = true
+		investigations_completed.emit()
 		
 func _all_creek_investigations_complete() -> bool:
 	return (
 		"creek_water_1" in completed_investigations
 		and "creek_water_2" in completed_investigations
 	)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
--- Game/scripts/interaction/investigation_point.gd
+++ Game/scripts/interaction/investigation_point.gd
@@ -2,10 +2,12 @@ extends Area2D
 signal investigated(investigation_id: String)
 
 var dog_nearby: bool = false
-var has_been_investigated: bool = false
 var dog: Node2D = null
+var investigation_count: int = 0
+var investigated_this_visit: bool = false
 
 @export_multiline var investigation_text: String = ""
+@export var repeat_investigation_texts: Array[String] = []
 @onready var interaction_prompt: Label = $InteractionPrompt
 @onready var investigation_label: RichTextLabel = $InvestigationText
 @export var text_display_duration: float = 5.0
@@ -15,12 +17,13 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("dog"):
 		dog = body
 		dog_nearby = true
-		if not has_been_investigated:
+		if _has_available_investigation_text():
 			interaction_prompt.show()
 					
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("dog"):
 		dog_nearby = false
+		investigated_this_visit = false
 		interaction_prompt.hide()
 		if not investigation_label.visible:
 			dog = null
@@ -31,18 +34,30 @@ func _process(_delta: float) -> void:
 	if interaction_prompt.visible and dog:
 		interaction_prompt.global_position = dog.global_position + Vector2(-45, -70)
 	
-	if dog_nearby and Input.is_action_just_pressed("investigate"):
+	if dog_nearby and not investigated_this_visit and _has_available_investigation_text() and Input.is_action_just_pressed("investigate"):
 		for point in get_tree().get_nodes_in_group("investigation_point"):
 			point.hide_investigation_text()
-		has_been_investigated = true
 		investigated.emit(investigation_id)
 		interaction_prompt.hide()
-		investigation_label.text = "[i]%s[/i]" % investigation_text
+		investigation_label.text = "[i]%s[/i]" % _get_current_investigation_text()
 		investigation_label.global_position = dog.global_position + Vector2(-60, -100)
 		investigation_label.show()
-		
+		investigation_count += 1
+		investigated_this_visit = true
 		await get_tree().create_timer(text_display_duration).timeout
 		investigation_label.hide()
+		if not dog_nearby:
+			dog = null
 
 func hide_investigation_text() -> void:
 	investigation_label.hide()
+	
+func _get_current_investigation_text() -> String:
+	if investigation_count == 0:
+		return investigation_text
+
+	var repeat_index: int = investigation_count - 1
+	return repeat_investigation_texts[repeat_index]
+	
+func _has_available_investigation_text() -> bool:
+	return investigation_count <= repeat_investigation_texts.size()
